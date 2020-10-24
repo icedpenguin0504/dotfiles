af7ecfc34cb8d784423394a776ee25d557c17858
icedpenguin0504 <swimpenguin0504@gmail.com>
2020-10-24 21:01:44 +0900

    add: Add commands that create symbolic links

---
+++ install.sh
@@ -0,0 +1,8 @@
+# Make symbolic links
+
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/.zshenv $HOME/.zshenv
+ln -s $HOME/dotfiles/.vim $HOME/.vim
+ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/.ideavimrc $HOME/.ideavimrc
+ln -s $HOME/dotfiles/.config $HOME/.config
