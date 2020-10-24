2150cc280e714743d3417d393a6ac8204f0186e5
icedpenguin0504 <swimpenguin0504@gmail.com>
2020-10-24 21:40:24 +0900

    add: Add commands that create symbolic links for NeoVim
    NeoVim use the same setting file as Vim

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ ln -s $HOME/dotfiles/.vim $HOME/.vim
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/.ideavimrc $HOME/.ideavimrc
 ln -s $HOME/dotfiles/.config $HOME/.config
+
+ln -s $HOME/dotfiles/.vim $HOME/dotfiles/.config/nvim/init.vim
+ln -s $HOME/dotfiles/.vim/coc-settings.json $HOME/dotfiles/.config/nvim/coc-settings.json
